0ca86ce69469148baddf25efd7e2c0df6047be09
Gamerface21 <Gamerface21@users.noreply.github.com>
2017-10-26 14:58:44 -0400

    Stack Unfinished 1

--- StackL.cpp
+++ StackL.cpp
@@ -1,1 +1,29 @@
 #include "StackL.h"
+
+int Stack:: size()
+{
+	
+}
+
+void Stack::push (int)
+{
+	
+}
+
+void Stack:: pop()
+{
+	
+}
+
+void Stack::top()
+{
+
+]
+
+void Stack::clear()
+{
+	while (num_elements >0)
+		{ 
+				remove(1);
+		}
+}
